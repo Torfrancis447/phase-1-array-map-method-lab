33a949cbc3f61ae1846ee222d12c290711cb0349
tor <torfrancis447@gmail.com>
2022-11-06 12:38:57 -0500

    commit tree e3b76d0d0402fa40ec46c8a9eca3809b58711304

--- index.js
+++ index.js
@@ -12,5 +12,34 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+//  function assign(tutorialSentence){
+//   const words = tutorialSentence.split(" ")
+//     function capitalize (word){
+//       const capitalLetter = word.slice(0,1).toUpperCase()
+//       const restOfWord= word.slice(1)
+//       return capitalLetter + restOfWord
+//     }
+//   const capitalWords = words.map(word => capitalize(word))
+  
+  
+//    return capitalWords.join(" ")
+//   } 
+  return tutorials.map((tutorialSentence) => {
+    const words = tutorialSentence.split(" ")
+      // function capitalize (word){
+      //   const capitalLetter = word.slice(0,1).toUpperCase()
+      //   const restOfWord= word.slice(1)
+      //   return capitalLetter + restOfWord
+      // }
+    const capitalWords = words.map((word) => {
+      const capitalLetter = word.slice(0,1).toUpperCase()
+      const restOfWord= word.slice(1)
+      return capitalLetter + restOfWord
+    })
+    
+    
+     return capitalWords.join(" ")
+    } )
 }
+
+titleCased();
